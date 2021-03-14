18cc3ca1494b063800f8c880e7bd4f2e7a3b4d68
aldrn <aldrn29@gmail.com>
2021-03-14 21:57:17 +0900

    navigation

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -28,7 +28,7 @@ class Home extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section className="contatiner">
+      <section className="container">
         {isLoading
           ? (
             <div className="loader">
